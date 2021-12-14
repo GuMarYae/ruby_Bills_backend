4606ab0f7853b92510b8ce55b7b8c9d64bf3d51a
GuMarYae <tddanielsjr@gmail.com>
2021-12-14 16:52:41 -0600

    ruby backend check

---
+++ db/migrate/20211214220644_create_bills.rb
@@ -0,0 +1,13 @@
+class CreateBills < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bills do |t|
+      t.string :title
+      t.string :date
+      # t.string :string
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
